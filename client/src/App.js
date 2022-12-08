8f69e021ae5459f7b203711740741146a82f2a7c
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-12-07 19:48:38 -0700

    feat: react router functionality, and basic navbar set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Navbar from './components/Nav/Navbar';
+import Posts from './components/pages/Posts';
+import Login from './components/pages/Login';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/posts' element={<Posts />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Nav/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+
+const Navbar = () => {
+  // const handleLogout = () => {
+  //   Auth.logout();
+  // };
+
+  const loggedIn = false;
+
+  return (
+    <div className='flex justify-around items-center'>
+      <div className='h-[7vh] mx-10'>
+        <h1 className='custom-logo text-black text-6xl'>hackit</h1>
+      </div>
+      <div>
+        <nav className='mx-10'>
+          <ul className='flex text-black'>
+            <li className='mx-4 uppercase'>
+              <Link to='/'>About</Link>
+            </li>
+
+            <li className='mx-4 uppercase'>
+              <Link to='/posts'>Posts</Link>
+            </li>
+            {loggedIn ? (
+              <li className='mx-4 uppercase' /* onClick={handleLogout} */>
+                <Link to='/'>Logout</Link>
+              </li>
+            ) : (
+              <li className='mx-4 uppercase'>
+                <Link to='/login'>Login</Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
