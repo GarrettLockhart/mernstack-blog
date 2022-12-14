d123b43e3582dc49d7cd9e90e5e42089eb127e7a
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-12-13 18:49:58 -0700

    style: temp random photo api for blog post styling

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaChevronRight } from 'react-icons/fa';
-import PostPlaceholder from '../../img/post-placeholder.jpeg';
+import axios from 'axios';
 
 const Home = () => {
+  const [imgURL, setImgURL] = useState('');
+  useEffect(() => {
+    axios
+      .get('https://picsum.photos/1000')
+      .then((response) => {
+        setImgURL(response.request.responseURL);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <div className='flex flex-col justify-start items-center h-[23vh]'>
@@ -17,25 +27,32 @@ const Home = () => {
       <div className='flex justify-center items-end h-[23vh] mb-20'>
         <Link
           to='/signup'
-          className='flex justify-center items-center mb-10 bg-main-200 hover:bg-main-300 text-white ease-in-out duration-200 px-5 py-3 rounded-full mx-2'
+          className='flex justify-center items-center mb-10 bg-main-200 hover:bg-main-300 text-white ease-in-out duration-200 px-5 py-3 rounded-full mx-2 shadow'
         >
           Get Started <FaChevronRight className='ml-2' />
         </Link>
         <Link
           to='/about'
-          className='flex justify-center items-center mb-10 bg-white hover:bg-gray-100 ease-in-out duration-200 px-5 py-3 rounded-full mx-2'
+          className='flex justify-center items-center mb-10 bg-white hover:bg-gray-100 ease-in-out duration-200 px-5 py-3 rounded-full mx-2 shadow'
         >
           Learn More <FaChevronRight className='ml-2' />
         </Link>
       </div>
-      <div className='container mx-auto'>
-        <section className='grid xl:grid-cols-3 grid-cols-1 p-10 w-4/5 mx-auto'>
-          <div className='col-span-1 mb-4'>
-            <img
+      <div className=''>
+        <section className='grid lg:grid-cols-3 md:grid-rows-2 grid-cols-1 p-10 w-4/5 mx-auto'>
+          <div
+            className='col-span-1 mb-4 w-full h-full'
+            style={{
+              backgroundImage: `url(${imgURL})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center'
+            }}
+          >
+            {/* <img
               src={PostPlaceholder}
               alt='Placeholder'
-              className='h-[95%] w-full rounded-xl'
-            />
+              className='h-[100%] w-full rounded-xl'
+            /> */}
           </div>
           <div className='col-span-2 flex flex-col px-10 h-full'>
             <section className='flex h-10 justify-start'>
@@ -47,7 +64,7 @@ const Home = () => {
               </p>
             </section>
             <section>
-              <h3 className='pt-4 text-3xl'>
+              <h3 className='pt-4 text-xl'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Corrupti, dicta.
               </h3>
@@ -63,7 +80,7 @@ const Home = () => {
                 Author: <Link to='/'>Garrett Lockhart</Link>{' '}
               </p>
             </section>
-            <button className='w-[25%] px-5 py-2 rounded-full bg-black text-white'>
+            <button className='lg:w-[50%] px-5 py-2 rounded-full bg-black text-white'>
               Full Post
             </button>
           </div>
