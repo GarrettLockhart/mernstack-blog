36b339edb796fe6e51252a90403e2a3a13b8e086
Garrett Lockhart <lockhartphotography1@gmail.com>
2023-01-22 08:58:33 -0700

    style: added blog card styling

--- client/src/components/pages/Blog.jsx
+++ client/src/components/pages/Blog.jsx
@@ -10,7 +10,7 @@ const Blog = () => {
       </div>
       <div className='container mx-auto'>
         <section
-          className='flex flex-col justify-end p-10 h-[600px] my-16'
+          className='flex flex-col justify-end p-10 h-[600px] my-16 custom-card'
           style={{
             backgroundImage: `url(${imgPlaceholder})`,
             backgroundSize: 'cover',
@@ -19,10 +19,20 @@ const Blog = () => {
         >
           <div className='bg-gray-200 rounded-lg py-3 px-5'>
             <h2 className='text-black text-xl font-bold'>Post Title</h2>
-            <p className='text-black text-md'>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
-              soluta quis optio. Provident fuga quae dolor impedit sunt
-              laudantium vero!
+            <p className='text-black text-md custom-para'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+              explicabo sed vel minus mollitia. Vitae mollitia delectus culpa
+              labore eius eos, est commodi ipsum fugiat quas? Nostrum ipsum iste
+              adipisci cupiditate? Laboriosam, itaque exercitationem rerum
+              cupiditate voluptatum laborum reprehenderit asperiores totam iusto
+              dignissimos quibusdam, excepturi ut omnis in reiciendis magnam!
+              Vero molestias officiis quaerat fugiat magni voluptas libero quasi
+              nesciunt sunt ducimus illo blanditiis harum ipsa dolore porro
+              voluptates voluptatum animi officia aut eligendi, possimus
+              dolores! Iste deserunt exercitationem dicta repellendus laboriosam
+              ducimus deleniti ratione officia, quod accusamus culpa facere
+              perferendis explicabo maiores accusantium, quis atque sapiente
+              consequuntur! Inventore, tenetur?
             </p>
           </div>
         </section>
