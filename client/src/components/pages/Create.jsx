6abc910375f5bff058604249b61459b535f8c7f0
Garrett Lockhart <lockhartphotography1@gmail.com>
2023-03-17 21:15:20 -0600

    fix: cleaned up axios POST request for creating posts

--- client/src/components/pages/Create.jsx
+++ client/src/components/pages/Create.jsx
@@ -4,41 +4,18 @@ import axios from 'axios';
 const Create = () => {
   const [post, setPost] = useState({ title: '', content: '' });
 
-  const headers = {
-    'Content-Type': 'application/json'
-  };
-
   const handleFormSubmit = (e) => {
     e.preventDefault();
     axios
-      .post(
-        '/api/post/create',
-        {
-          title: post.title,
-          content: post.content
-        },
-        headers
-      )
+      .post('/api/post/create', {
+        title: post.title,
+        content: post.content
+      })
       .then((response) => {
         console.log(response);
       })
-      .catch(function (error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
+      .catch((err) => {
+        console.log(`Something went wrong: ${err}`);
       });
   };
 
